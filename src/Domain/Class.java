43eb94db2d1ca62894e446dbeb3cacbe3c236f92
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-10-17 15:55:12 +0200

    Class Class (great)

---
+++ src/Domain/Class.java
@@ -0,0 +1,23 @@
+/**
+ * Made by: Roberto
+ */
+
+package Domain;
+
+public class Class {
+	/**
+	 * Data Structures
+	 */
+
+	/**
+	 * Attributes
+	 */
+
+	/**
+	 * Constructor / Destructor
+	 */
+
+	/**
+	 * Getters / Setters
+	 */
+}
