7e7f2c7e510cf1d71bde1bd74e54d684ece6c1e8
Alberto <algimeneza@gmail.com>
2018-10-31 10:12:32 +0100

    Minor changes on restrictions

--- src/Domain/Restriction.java
+++ src/Domain/Restriction.java
@@ -45,26 +45,29 @@ public class Restriction {
             Class c = classes.get(i);
             DateTime cDT = c.getDateTime();
             Classroom cClassroom = c.getClassroom();
+            // if class c has a classroom and date assigned
             if(cDT != null && cClassroom != null){
                 String wday = cDT.getWeekday().toString();
                 Integer shour = cDT.getStartHour();
                 Integer duration = cDT.getDuration();
                 String classroom = cClassroom.getName();
+                // inspect every other class cj
                 for(int j=0; j<classes.size(); ++j){
                     Class cj = classes.get(j);
                     DateTime cjDT = cj.getDateTime();
                     Classroom cjClassroom = cj.getClassroom();
+                    // possible conflict if both classes have the same classroom and week day
                     if(i != j && cjDT != null && cjClassroom != null){
                         if(wday == cjDT.getWeekday().toString() && classroom == cjClassroom.getName()){
                             int jShour = cjDT.getStartHour();
                             int jDuration = cjDT.getDuration();
+                            //conflict if overlapped
                             if((jShour >= shour && jShour < shour+duration) ||
                                     (jShour+jDuration >= shour && jShour+jDuration < shour+duration) ||
                                     (jShour< shour && jShour+jDuration > shour+duration)) return false;
                         }
                     }
                 }
-
             }
         }
         return true;
