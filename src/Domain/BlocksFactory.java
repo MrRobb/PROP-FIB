4e0136a6f914bae16c7aec7f9dc34c9ebbf30952
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-11-02 18:48:01 +0100

    Blocks factory

---
+++ src/Domain/BlocksFactory.java
@@ -0,0 +1,9 @@
+package Domain;
+
+public class BlocksFactory {
+
+	public static boolean produce() {
+		Blocks.getInstance().add("No class between time frame", Block::noClassOnDayBetweenSHEH);
+		Blocks.getInstance().add("No overlapping classroom and time", Block::noTwoClassesAtSameHourSameClassroom);
+	}
+}
