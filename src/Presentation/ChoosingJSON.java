0e380557b54d1263861c1bcfa6cf982f411e7bb7
Alberto <algimeneza@gmail.com>
2018-11-28 09:56:02 +0100

    Choosing JSON FX

---
+++ src/Presentation/ChoosingJSON.java
@@ -0,0 +1,27 @@
+package Presentation;
+
+import javafx.event.Event;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChoosingJSON implements Initializable {
+    public SplitMenuButton fileMenuButton;
+    public Button btnFIBComplete;
+    public Button btnFIBMand;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void pressFIBcompletedButton(Event e){
+
+    }
+    public void pressFIBmandButton(Event e){
+
+    }
+
+}
